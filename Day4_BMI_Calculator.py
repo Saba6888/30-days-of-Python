34ca3837620a07ce21148da5b825f3d1c14fbb42
Saba Aafreen <112338333+Saba6888@users.noreply.github.com>
2024-09-03 15:15:52 +0530

    Update Day4_BMI_Calculator.py

--- Day4_BMI_Calculator.py
+++ Day4_BMI_Calculator.py
@@ -1,23 +1,27 @@
-def calculate_bmi(weight, height):
-    bmi = weight / (height ** 2)
+#Created a function named bmi and passed two arguments height and weight
+def calculator_bmi(weight,height):
+    bmi=weight/(height**2)
     return bmi
 
-def determine_bmi_category(bmi):
-    if bmi < 18.5:
-        return 'Underweight'
-    elif 18.5 <= bmi < 24.9:
-        return 'Normal weight'
-    elif 25 <= bmi < 29.9:
-        return 'Overweight'
+#created a function for bmi and its conditions using if else and conditional operators
+def bmi_category(bmi):
+    if bmi<18.5:
+        return "underweight"
+    elif bmi>=18.5 and bmi<24.9:
+        return "Normal"
+    elif bmi>=24.9 and bmi<29.9:
+        return "Overweight"
     else:
-        return 'Obesity'
+        return "Obese"
 
-def main():
-    weight = float(input("Enter your weight in kilograms: "))
-    height = float(input("Enter your height in meters: "))
-    bmi = calculate_bmi(weight, height)
-    category = determine_bmi_category(bmi)
-    print(f"\nYour BMI is: {bmi:.2f}")
-    print(f"You are classified as: {category}")
-if __name__ == "__main__":
+#Created a main function to call functions amd print the final values
+def main()
+    weight=float("Enter weight in kgs: ")
+    height=float("Enter height in ms: ")
+
+    bmi=calculator_bmi(weight,height)
+    category=bmi_category(bmi)
+    print(f"\nYour bmi is: {bmi:.2f}")
+    print(f"\nYou are of category: {category}")
+if __name__=="__main__":
     main()

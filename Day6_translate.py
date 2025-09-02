f65fd18f44cd7a39519f2d76bd76510fee66e9b1
Saba Aafreen <saafreen277@gmail.com>
2025-09-02 18:53:24 +0530

    Add all folders

--- Day6_translate.py
+++
@@ -1,9 +0,0 @@
-def translate(phrase):
-    translation=""
-    for letter in  phrase:
-        if letter in "AEIOUaeiou":
-            translation=translation+"abg"
-        else:
-            translation=translation+letter
-    return translation
-print(translate(input("Enter a phrase: ")))

42a07950ff7702a175d57a69c2005fe9a0e0ed26
Saba Aafreen <saafreen277@gmail.com>
2025-09-02 19:31:59 +0530

    Deleting existing folders

--- Day11_Word_Counter.py
+++
@@ -1,7 +0,0 @@
-def text_count(text):
-    word=text.split()  # text.split function to split each word based on spaces
-    return len(word)   #return length of list of words
-
-sentence=input("Enter the sentence: ")
-word_count=text_count(sentence)    #called the function
-print(f"The number of words in a given sentence is:  {word_count}")
